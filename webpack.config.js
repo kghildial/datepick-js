2aa1703158f7818bb5b664a5beb97179253cd324
Kinshuk Ghildial <kinshuk.ghildial@kuliza.com>
2019-12-20 15:01:19 +0530

    Add ES6 + Prettier support

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,22 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.scss$/i,
         use: [
           'style-loader',
           'css-loader',
           'sass-loader',
         ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   }
