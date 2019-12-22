0aade888cd831913fa29971982e52072ce7df757
Kinshuk Ghildial <kinshuk.ghildial@kuliza.com>
2019-12-22 12:26:01 +0530

    Add calender + it's helper methods

--- src/index.js
+++ src/index.js
@@ -1,3 +1,134 @@
+/**
+ * All the source JS for datepick-js
+ */
+
+// import for the datepick-js styles
 import './index.scss';
 
-console.log('Yoyoyoyo!!!');
+// Zeller Month Map
+const monthsMap = {
+  Jan: 11,
+  Feb: 12,
+  Mar: 1,
+  Apr: 2,
+  May: 3,
+  Jun: 4,
+  Jul: 5,
+  Aug: 6,
+  Sep: 7,
+  Oct: 8,
+  Nov: 9,
+  Dec: 10,
+};
+
+// Zeller days map
+const daysList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+const monthsList = Object.keys(monthsMap);
+
+function initDatepicker(elememtID, dpjsConfig) {
+  // Attach the initial datepicker scaffold
+  document.querySelector('body').innerHTML +=
+    '<div id="dpjs_datepicker"><div class="dpjs_selectors"><select name="dpjs_month" id="dpjs_monthSelector"></select><select name="dpjs_year"></select></div><div id="dpjs_calender"><div id="dpjs_days"></div><div id="dpjs_dates"></div></div></div>';
+
+  // Populate days in the calender section
+  populateCalenderDays();
+  populateMonthDates('Dec', 2019);
+}
+
+// Zeller Magic :P
+const getMonthStartingDayIndex = (month, year) => {
+  const K = 1;
+  const M = monthsMap[month];
+  const D =
+    [11, 12].indexOf(M) !== -1
+      ? Number(String(year).slice(2, 4)) - 1
+      : Number(String(year).slice(2, 4));
+  const C = Number(String(year).slice(0, 2));
+
+  const F =
+    K +
+    Math.floor((13 * M - 1) / 5) +
+    D +
+    Math.floor(D / 4) +
+    Math.floor(C / 4) -
+    2 * C;
+
+  const remainder = F < 0 ? 7 + (F % 7) : F % 7;
+
+  return remainder;
+};
+
+function isLeapYear(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) return true;
+
+      return false;
+    }
+
+    return true;
+  }
+}
+
+function getTotalDaysInMonth(month, year) {
+  // Check for Feb Leap year condition
+  if (month === 'Feb') {
+    if (isLeapYear(year)) return 29;
+
+    return 28;
+  }
+
+  const monthIndex = monthsList.indexOf(month);
+
+  if (monthIndex <= 6) return monthIndex % 2 === 0 ? 31 : 30;
+
+  return monthIndex % 2 !== 0 ? 31 : 30;
+}
+
+function populateCalenderDays() {
+  daysList.forEach(day => {
+    document.querySelector(
+      '#dpjs_days',
+    ).innerHTML += `<p id="${day}">${day}</p>`;
+  });
+}
+
+function populateMonthDates(month, year) {
+  // Clear any existing dates in the calender
+  document.querySelector('#dpjs_dates').innerHTML = '';
+  const startingDayIndex = getMonthStartingDayIndex(month, year);
+  const totalDaysInMonth = getTotalDaysInMonth(month, year);
+
+  let dateValue = 1;
+  let rowValue = 1;
+  let startPopulatingDates = false;
+
+  while (dateValue <= totalDaysInMonth) {
+    document.querySelector(
+      '#dpjs_dates',
+    ).innerHTML += `<div id="dpjs_datesRow-${rowValue}" class="dpjs_datesList"></div>`;
+    for (let i = 0; i < 7; i++) {
+      if (i === startingDayIndex) startPopulatingDates = true;
+
+      if (startPopulatingDates && dateValue <= totalDaysInMonth) {
+        // add <p> tag with date
+        document.querySelector(
+          `#dpjs_datesRow-${rowValue}`,
+        ).innerHTML += `<p id="dpjs_date-${dateValue}">${dateValue}</p>`;
+        dateValue++;
+      } else {
+        // add empty <p> tag
+        document.querySelector(
+          `#dpjs_datesRow-${rowValue}`,
+        ).innerHTML += `<p class="dpjs_emptyDateSlot"></p>`;
+      }
+    }
+    if (dateValue <= totalDaysInMonth) rowValue++;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', initDatepicker);
+
+// NOTE: Commented temporarily for testing on local HTML
+// export default initDatepicker;
